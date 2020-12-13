4df8fb3c1035150f19481b6474ab49e14c7a34c0
Rodrigo <rodrigoresendemoreira@gmail.com>
2020-12-13 17:38:05 -0300

    add heroes mock list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -6,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero = 'Windstorm';
+  
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
